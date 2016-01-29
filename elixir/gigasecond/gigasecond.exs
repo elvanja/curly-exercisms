4d9efc194927e868ebff4126b6edef5011dde52c
Vanja Radovanović <elvanja@gmail.com>
2016-01-29 15:15:58 +0100

    optimized function args

--- elixir/gigasecond/gigasecond.exs
+++ elixir/gigasecond/gigasecond.exs
@@ -3,9 +3,8 @@ defmodule Gigasecond do
 	Calculate a date one billion seconds after an input date.
 	"""
 	@spec from({{pos_integer, pos_integer, pos_integer}, {pos_integer, pos_integer, pos_integer}}) :: :calendar.datetime
-
-  def from({{year, month, day}, {hours, minutes, seconds}}) do
-    {{year, month, day}, {hours, minutes, seconds}}
+  def from(datetime) do
+    datetime
       |> :calendar.datetime_to_gregorian_seconds
       |> +(1_000_000_000)
       |> :calendar.gregorian_seconds_to_datetime
