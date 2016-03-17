75d19825af1cf2cf97e80f882ea120ced73692cf
Vanja Radovanović <elvanja@gmail.com>
2016-03-17 14:56:00 +0100

    minor refactoring to improve readability

--- elixir/dot-dsl/dot.exs
+++ elixir/dot-dsl/dot.exs
@@ -10,15 +10,7 @@ defmodule Dot do
   defp build_graph({:__block__, _, graphs}) do
     graphs
     |> Enum.map(&(&1 |> build_graph |> Code.eval_quoted |> elem(0)))
-    |> Enum.reduce(%Graph{}, fn(graph, acc) ->
-      graph |> Map.merge(acc, fn(k, v1, v2) ->
-        if k == :__struct__ do
-          v1
-        else
-          v1 ++ v2 |> Enum.sort
-        end
-      end) 
-    end)
+    |> Enum.reduce(%Graph{}, &merge_graphs_sorted/2)
     |> Macro.escape
   end
 
@@ -51,5 +43,15 @@ defmodule Dot do
 
   defp build_graph(_) do
     raise ArgumentError
+  end
+
+  defp merge_graphs_sorted(first, second) do
+    Map.merge(first, second, fn(k, v1, v2) ->
+      if k == :__struct__ do
+        v1
+      else
+        v1 ++ v2 |> Enum.sort
+      end
+    end)
   end
 end
